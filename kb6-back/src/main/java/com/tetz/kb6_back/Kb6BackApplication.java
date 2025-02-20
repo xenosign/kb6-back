5a51d38fa226f528b82e70d7cdf951351abe5fcf
kdtTetz <xenosign@naver.com>
2025-02-20 13:40:26 +0900

    Initial commit

---
+++ kb6-back/src/main/java/com/tetz/kb6_back/Kb6BackApplication.java
@@ -0,0 +1,13 @@
+package com.tetz.kb6_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Kb6BackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Kb6BackApplication.class, args);
+	}
+
+}
---
+++ kb6-back/src/test/java/com/tetz/kb6_back/Kb6BackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tetz.kb6_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Kb6BackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
